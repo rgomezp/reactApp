825083596f64a27ed3d2f91e9aac5d05c8a03892
Rodrigo Gomez Palacio <rogomezpalacio@gmail.com>
2018-07-29 17:08:44 -0700

    First commit with TodoList & ToDo components

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h3>React lives!</h3>,
+var dummyData = [
+  "Do laundry", "Clean kitchen", "Go shopping", "Call Mom", "Walk the dog", "Feed the fish", "Take out the trash", "Practice Violin", "Clean the car"
+];
+
+class ToDo extends React.Component{
+  render(){
+    return (<div><button>X</button>{this.props.task}</div>)
+  }
+}
+
+class TodoList extends React.Component{
+  render(){
+    return(
+      <ul>{dummyData.map((todo)=><li><ToDo task={todo}/></li>)}</ul>
+    );
+  }
+}
+
+ReactDOM.render(<div><TodoList/></div>,
    document.getElementById('root'));
